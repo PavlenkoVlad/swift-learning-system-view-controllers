62ac55d6f58e656808faf0fe66bab7b6b9688e03
Vlad Pavlenko <pavlenko0902@gmail.com>
2019-07-09 15:53:55 +0300

    Initial Commit

---
+++ SystemViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by Владислав Павленко on 7/9/19.
+//  Copyright © 2019 Владислав Павленко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
