d4db1c0839563332f1914b99447b2062320b92d9
Vlad Pavlenko <pavlenko0902@gmail.com>
2019-07-19 17:42:43 +0300

    init commit

--- SystemViewControllers/ViewController.swift
+++ SystemViewControllers/ViewController.swift
@@ -7,14 +7,104 @@
 //
 
 import UIKit
+import SafariServices
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MFMailComposeViewControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var safariButton: UIButton!
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var emailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setButtonLayoutProperties()
     }
-
-
+    
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        guard let image = imageView.image else { return }
+        
+        let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = sender
+        
+        present(activityViewController, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+        if let url = URL(string: "https://apple.com") {
+            let safariViewController = SFSafariViewController(url: url)
+            present(safariViewController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let alertController = UIAlertController(title: "Choose Image Source", message: nil, preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let newPhotoAction = UIAlertAction(title: "New Photo", style: .default, handler: { action in
+                imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true, completion: nil)
+            })
+            alertController.addAction(newPhotoAction)
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: { action in
+            imagePicker.sourceType = .photoLibrary
+            self.present(imagePicker, animated: true, completion: nil)
+            })
+            alertController.addAction(photoLibraryAction)
+        }
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func viewEmailButtonTapped(_ sender: UIButton) {
+        guard MFMailComposeViewController.canSendMail() else { return }
+        
+        let mailComposer = MFMailComposeViewController()
+        mailComposer.mailComposeDelegate = self
+        
+        mailComposer.setToRecipients(["example@example.com"])
+        mailComposer.setSubject("Look at this")
+        mailComposer.setMessageBody("Hello World", isHTML: false)
+        
+        present(mailComposer, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = selectedImage
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setButtonLayoutProperties() {
+        let borderColor = UIColor(red: 0, green: 0.478431, blue: 1, alpha: 1).cgColor
+        
+        shareButton.layer.borderWidth = 1.0
+        shareButton.layer.borderColor = borderColor
+        safariButton.layer.borderWidth = 1.0
+        safariButton.layer.borderColor = borderColor
+        cameraButton.layer.borderWidth = 1.0
+        cameraButton.layer.borderColor = borderColor
+        emailButton.layer.borderWidth = 1.0
+        emailButton.layer.borderColor = borderColor
+    }
 }
 
